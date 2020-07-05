777a51b023d7cf4ba62b70c4d2799df8a3237948
yousuke86 <yousuke7201@yahoo.co.jp>
2020-07-05 15:14:15 +0900

    deploy_basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+# Basic認証の実装
+  before_action :basic_auth
+# Basic認証を本番環境でのみ適用
+  before_action :basic_auth, if: :production?
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
+    end
+  end
+
+  def production?
+    Rails.env.production?
+  end
+
 end
