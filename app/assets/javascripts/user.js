ee30beb055bb85595f54e27efe8771b952b15a90
yousuke86 <yousuke7201@yahoo.co.jp>
2020-07-15 15:30:50 +0900

    user_mypage_add_js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,4 +1,31 @@
-// jqueryが読み込めてるかのテストです
 $(function(){
-  console.log('OK');
+  // ユーザーマイページの「出品した商品 - 出品中」のjs実装
+  $('.exhibision-now-info').hide();
+
+  $(function(){
+    $('.exhibision-now').on('click', function(e){
+      e.preventDefault();
+      $('.exhibision-now-info').toggle(350);
+    });
+  });
+
+  // ユーザーマイページの「出品した商品 - 売却済み」のjs実装
+  $('.exhibision-end-info').hide();
+
+  $(function(){
+    $('.exhibision-end').on('click', function(e){
+      e.preventDefault();
+      $('.exhibision-end-info').toggle(350);
+    });
+  });
+
+   // ユーザーマイページの「購入した商品 - 過去の取引」のjs実装
+   $('.purchase-info').hide();
+
+   $(function(){
+     $('.purchase').on('click', function(e){
+       e.preventDefault();
+       $('.purchase-info').toggle(350);
+     });
+   });
 });
