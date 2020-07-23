f738c7c81e4cb83d92d5a0006b6b5abaf9f724c0
yousuke86 <yousuke7201@yahoo.co.jp>
2020-07-23 22:49:10 +0900

    make-item_exhibition_page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root 'items#index'
+  resources :items, only: [:new]
   resources :users, only: [:show]
   resources :cards, only: [:new, :create]
   
