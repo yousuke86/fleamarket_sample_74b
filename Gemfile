d506dc9b5ff718dfe69788e8e3b77960a16b1c3d
moritasan <kumasan-desu1@docomo.ne.jp>
2020-07-29 15:42:18 +0900

    make_rspec_test_morita_suzuki

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
+  gem 'factory_bot_rails'
+  gem 'rspec-rails'
   gem 'pry-rails'
   gem 'capistrano'
   gem 'capistrano-rbenv'
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,81 @@
+require 'rails_helper'
+describe User do
+  describe '#create' do
+    it "nickname、email、passwordとpassword_confirmation、last_name、first_name、last_name_kana、first_name_kana、birth_dayが存在すれば登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it "nicknameがない場合は登録できないこと" do
+      user = build(:user, nickname: nil)
+      user.valid?
+      expect(user.errors[:nickname]).to include("を入力してください")
+    end
+
+    it "emailがない場合は登録できないこと" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "passwordがない場合は登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    it "passwordが存在してもpassword_confirmationがない場合は登録できないこと" do
+      user = build(:user, password_confirmation: nil)
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include
+    end
+
+    it "last_nameがない場合は登録できないこと" do
+      user = build(:user, last_name: nil)
+      user.valid?
+      expect(user.errors[:last_name]).to include("を入力してください")
+    end
+
+    it "first_nameがない場合は登録できないこと" do
+      user = build(:user, first_name: nil)
+      user.valid?
+      expect(user.errors[:first_name]).to include("を入力してください")
+    end
+
+    it "last_name_kanaがない場合は登録できないこと" do
+      user = build(:user, last_name_kana: nil)
+      user.valid?
+      expect(user.errors[:last_name_kana]).to include("を入力してください")
+    end
+
+    it "first_name_kanaがない場合は登録できないこと" do
+      user = build(:user, first_name_kana: nil)
+      user.valid?
+      expect(user.errors[:first_name_kana]).to include("を入力してください")
+    end
+
+    it "birth_dayがない場合は登録できないこと" do
+      user = build(:user, birth_day: nil)
+      user.valid?
+      expect(user.errors[:birth_day]).to include("を入力してください")
+    end
+
+    it "重複したemailが存在する場合登録できないこと" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
+    end
+
+    it " passwordが6文字以上であれば登録できること " do
+      user = build(:user, password: "000000", password_confirmation: "000000")
+      expect(user).to be_valid
+    end
+
+    it " passwordが5文字以下であれば登録できないこと " do
+      user = build(:user, password: "00000", password_confirmation: "00000")
+      user.valid?
+      expect(user.errors[:password]).to include("は6文字以上で入力してください")
+    end
+  end
+end
