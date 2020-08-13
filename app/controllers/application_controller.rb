742811e3e7a947ea67f9572b6e89441c0330fdd0
yousuke86 <yousuke7201@yahoo.co.jp>
2020-08-13 17:10:49 +0900

    make-devise-edit-page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day])
   end
 
 # Basic認証の実装
