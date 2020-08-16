50123e9a0de6e1982980edd8f38aa7514e0e144a
ssuzukin <suzukin0318@gmail.com>
2020-08-16 14:35:13 +0900

    modify items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,10 +37,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  
-
-  
-
   def show
     @user = User.find(@item.seller_id)
     @status = @item.status
@@ -58,11 +54,31 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
-  
-
-  
 
   def purchase
+    card = Card.where(user_id: current_user.id).first
+    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
+    if card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      redirect_to new_card_path, notice: "クレジットカード情報を入力してください"
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      #保管した顧客IDでpayjpから情報取得
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+
+  def pay
+    card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp::Charge.create(
+      :amount => 500, #支払金額を入力（itemテーブル等に紐づけても良い）
+      :customer => card.customer_id, #顧客ID
+      :currency => 'jpy', #日本円
+    )
+    redirect_to root_path, notice: "支払いが完了しました"
   end
 
 
