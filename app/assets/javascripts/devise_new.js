b43bd3b0b86771c53a928a07a33ddfd002eed1ec
ssuzukin <suzukin0318@gmail.com>
2020-08-22 16:05:35 +0900

    edit address.js and devise_new.js

--- app/assets/javascripts/devise_new.js
+++ app/assets/javascripts/devise_new.js
@@ -1,78 +1,93 @@
-$(function() {
-  $('.account-page').on('click', function(e){
-    let nickname = $("#input-nickname").val();
-    let email = $("#input-email").val();
-    let password = $("#input-password").val();
-    let password_confirmation = $("#input-password_confirmation").val();
-    let first_name = $("#input-first_name").val();
-    let last_name = $("#input-last_name").val();
-    let first_name_kana = $("#input-first_name_kana").val();
-    let last_name_kana = $("#input-last_name_kana").val();
-    let year = $("#input-year").val();
-    let month = $("#input-month").val();
-    let day = $("#input-day").val();
-
-    if (nickname == '') {
-      $('#result-nickname').show();
-    }
-    else {
-      $('#result-nickname').hide();
-    };
-
-    if (email == '') {
-      $('#result-email').show();
-    }
-    else {
-      $('#result-email').hide();
-    };
-
-    if (password == '') {
-      $('#result-password').show();
-    }
-    else {
-      $('#result-password').hide();
-    };
-
-    if (password_confirmation == '') {
-      $('#result-password_confirmation').show();
-    }
-    else {
-      $('#result-password_confirmation').hide();
-    };
-
-    if (first_name == '') {
-      $('#result-first_name').show();
-    }
-    else {
-      $('#result-first_name').hide();
-    };
-
-    if (last_name == '') {
-      $('#result-last_name').show();
-    }
-    else {
-      $('#result-last_name').hide();
-    };
-
-    if (first_name_kana == '') {
-      $('#result-first_name_kana').show();
-    }
-    else {
-      $('#result-first_name_kana').hide();
-    };
-
-    if (last_name_kana == '') {
-      $('#result-last_name_kana').show();
-    }
-    else {
-      $('#result-last_name_kana').hide();
-    };
-
-    $(function(){
-      $('.field__birth-select').on('click', function(e){
-        $('#result-birth').hide();
-      });
+$(document).on('turbolinks:load', function() {
+  $(function() {
+    $('#input-nickname').on('blur', function(){
+      let nickname = $(this).val();
+      if (nickname == '') {
+        $('#result-nickname').show();
+      }
+      else {
+        $('#result-nickname').hide();
+      };
     });
 
+    $('#input-email').on('blur', function(){
+      let email = $(this).val();
+      if (email == '') {
+        $('#result-email').show();
+      }
+      else {
+        $('#result-email').hide();
+      };
+    });
+    
+    $('#input-password').on('blur', function(){
+      let password = $(this).val();
+      if (password == '') {
+        $('#result-password').show();
+      }
+      else {
+        $('#result-password').hide();
+      };
+    });
+
+    $('#input-password_confirmation').on('blur', function(){
+      let password_confirmation = $(this).val();
+      if (password_confirmation == '') {
+        $('#result-password_confirmation').show();
+      }
+      else {
+        $('#result-password_confirmation').hide();
+      };
+    });
+
+    $('#input-first_name').on('blur', function(){
+      let first_name = $(this).val();
+      if (first_name == '') {
+        $('#result-first_name').show();
+      }
+      else {
+        $('#result-first_name').hide();
+      };
+    });
+
+    $('#input-last_name').on('blur', function(){
+      let last_name = $(this).val();
+      if (last_name == '') {
+        $('#result-last_name').show();
+      }
+      else {
+        $('#result-last_name').hide();
+      };
+    });
+
+    $('#input-first_name_kana').on('blur', function(){
+      let first_name_kana = $(this).val();
+      if (first_name_kana == '') {
+        $('#result-first_name_kana').show();
+      }
+      else {
+        $('#result-first_name_kana').hide();
+      };
+    });
+
+    $('#input-last_name_kana').on('blur', function(){
+      let last_name_kana = $(this).val();
+      if (last_name_kana == '') {
+        $('#result-last_name_kana').show();
+      }
+      else {
+        $('#result-last_name_kana').hide();
+      };
+    });
+
+    $('#input-birth_day').on('blur', function(){
+      let birth_day = $(this).val();
+      if (birth_day == '') {
+        $('#result-birth').show();
+      }
+      else {
+        $('#result-birth').hide();
+      };
+    });
   });
 });
